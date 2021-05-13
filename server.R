2dc649f57b5ae7b552fd199891d424ae0920e3e0
mkaranja <m.karanja@cgiar.org>
2021-05-13 15:59:28 +0300

    updated

--- server.R
+++ server.R
@@ -1,9 +1,9 @@
 
 source("tabs/server/overview_server.R", local=T)
 source("tabs/server/data_server.R", local=T)
-# source("tabs/server/status_server.R", local=T)
+source("tabs/server/status_server.R", local=T)
 source("tabs/server/tc_server.R", local=T)
-# source("tabs/server/labels_server.R", local=T)
+source("tabs/server/labels_server.R", local=T)
 
 
 # Define server logic 
@@ -14,8 +14,8 @@ shinyServer(
   
     overviewserver(env_serv)
     dataserver(env_serv)
-    # statusserver(env_serv)
+    statusserver(env_serv)
     tc_server(env_serv)
-    # labels_server(env_serv)
+    labels_server(env_serv)
   
 })
